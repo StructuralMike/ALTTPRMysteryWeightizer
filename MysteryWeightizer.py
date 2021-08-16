04d867897d747be28f19423a97f772180d13a126
StructuralMike <66819228+StructuralMike@users.noreply.github.com>
2021-08-16 12:06:22 +0200

    some of Ezzelin's requests

--- MysteryWeightizer.py
+++ MysteryWeightizer.py
@@ -5,20 +5,25 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('--i', help='Path to the points weights file to use for rolling game settings')
+    parser.add_argument('--o', help='Output path for the rolled mystery yaml')
     args = parser.parse_args()
 
     if args.i:
         input_yaml = args.i
     else:
-        input_yaml = "mystery_example.yml"
+        input_yaml = "weightizer_example.yml"
+
+    if args.o:
+        output_file = args.o
+    else:
+        output_file = "mystery_weighted.yaml"
+
     with open(input_yaml, "r", encoding='utf-8') as f:
         yaml_weights = yaml.load(f, Loader=yaml.SafeLoader)
 
     while True:
         settings = {}
         points = 0
-
-        settings['description'] = yaml_weights['description']
 
         for setting,alternatives in yaml_weights['world'].items():
             options = [x for x in list(alternatives.items()) if type(x[1]) == int]
@@ -56,16 +61,19 @@ def main():
         settings['startinventory'] = {}
         start_items = random.randint(yaml_weights['weight_options']['min_starting_items'],yaml_weights['weight_options']['max_starting_items'])
         item_choices = []
-        for setting,alternatives in yaml_weights['startinventory'].items():
-            if 'on' in alternatives and type(alternatives['on']) == int:
-                item_choices.append(setting)
-        random.shuffle(item_choices)
-        while start_items > len(settings['startinventory']):
-            item = item_choices.pop()
-            options = [x for x in list(yaml_weights['startinventory'][item].items()) if type(x[1]) == int]
-            option = random.choice(options)
-            settings['startinventory'][item] = {option[0]: 1}
-            points += option[1]
+        try:
+            for setting,alternatives in yaml_weights['startinventory'].items():
+                if 'on' in alternatives and type(alternatives['on']) == int:
+                    item_choices.append(setting)
+            random.shuffle(item_choices)
+            while start_items > len(settings['startinventory']):
+                item = item_choices.pop()
+                options = [x for x in list(yaml_weights['startinventory'][item].items()) if type(x[1]) == int]
+                option = random.choice(options)
+                settings['startinventory'][item] = {option[0]: 1}
+                points += option[1]
+        except:
+            pass # No (more) starting items available
 
         settings['rom'] = {}
         for setting,alternatives in yaml_weights['rom'].items():
@@ -77,9 +85,10 @@ def main():
         if points <= yaml_weights['weight_options']['max_points'] and points >= yaml_weights['weight_options']['min_points']:
             break
 
-    print('Generated settings scoring {} points'.format(points))
+    settings['description'] = 'Weightizer score: {} points'.format(points)
 
-    output_file = "mystery_weighted.yaml"
+    print('Successfully generated mystery settings: {}'.format(output_file))
+
     with open(output_file, "w+", encoding='utf-8') as f:
         yaml.dump(settings,f, Dumper=yaml.SafeDumper, encoding='utf-8', allow_unicode=True, default_flow_style=False)
 
